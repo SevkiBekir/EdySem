4d842bb14ab3a402950110b836a233bdd879911a
Sevki Bekir Kocadag <bekirsevki@gmail.com>
2016-04-06 19:21:04 +0300

    fixed bugs

--- application/controllers/dbCreateModels.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class dbCreateModels extends CI_Controller {
-    
-	public function index(){
-		$this->load->model('dbModelCreator/createModels');
-	}
-}
