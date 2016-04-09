faf4031d4aac5bb1da797167d61f836b36067379
Sevki Bekir Kocadag <bekirsevki@gmail.com>
2016-04-09 20:20:55 +0300

    logout note added

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -3,10 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class logout extends CI_Controller {
 
+	//BAKILMASI LAZIM...
 	public function index(){
         
 		$this->session->sess_destroy();
         session('userId', ''); // Neden anlayamadım, session destroy bir sonraki yenilemede session datasını siliyor gibi :(
         loadView("main");
+        
 	}
 }
