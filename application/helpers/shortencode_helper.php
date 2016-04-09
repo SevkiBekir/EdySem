157613b818c75da14548d87c9637ba7a9e579127
Sevki Bekir Kocadag <bekirsevki@gmail.com>
2016-04-09 19:49:35 +0300

    new updated

---
+++ application/helpers/shortencode_helper.php
@@ -0,0 +1,86 @@
+<?php
+    /**
+    * Asim Dogan NAMLI
+    * asim.dogan.namli@gmail.com
+    * @2016
+    * 
+    * Bu scriptde ki asıl amacım, object oriented yazacağız derken uzadı uzadıya yazdığımız yapıları kısaltıp
+    * proje içinde kısa fonksiyonlarla daha etkin kullanabilmek
+    */
+    
+    /**
+     * $this->session->userData($key) yapısını kısaltır.
+     * 
+     * @param obj $key çekmek istenilen veri
+     */
+    function session($key, $val = NULL){
+        if($val){
+            get_instance()->session->set_userData($key, $val);
+        }
+        return get_instance()->session->userData($key);
+    }
+
+    /**
+     * /assets dizinine kısa yoldan erişebilirlik
+     * 
+     * @param  integer [$echo = 1] url bastırılsınmı bastırılmasın mı?
+     * @return string assets/ url'i
+     */
+    function assetsUrl($echo = 1){
+        if($echo){
+            echo get_instance()->config->base_url('assets', NULL).'/';
+        }
+        else{
+            return get_instance()->config->base_url('assets', NULL).'/';
+        }
+    }
+    
+    /**
+     * /assets dizinine kısa yoldan erişebilirlik
+     * 
+     * @param  integer [$echo = 1] return base url bastırılsınmı bastırılmasın mı?
+     * @return string assets/ url'i
+     */
+    function baseUrl($echo = 1){
+        if($echo){
+            echo get_instance()->config->base_url('', NULL);
+        }
+        else{
+            return get_instance()->config->base_url('', NULL);
+        }
+    }
+
+    /**
+     * $this->input->get yordamını xss kontrolü açık olarak döndrür
+     * 
+     * @param string $key Boş bırakıldığında tüm get verisini döndürür
+     */
+    function get($key = NULL){
+        return get_instance()->input->get($key, TRUE);
+    }
+
+    /**
+     * $this->input->post yordamını xss kontrolü açık olarak döndrür
+     * 
+     * @param string $key Boş bırakıldığında tüm post verisini döndürür
+     */
+    function post($key = NULL){
+        return get_instance()->input->post($key, TRUE);
+    }
+
+    /**
+     * $this->input->server verisini döndürür
+     * 
+     * @param string $key Boş bırakıldığında tüm server verisini döndürür
+     */
+    function server($key = NULL){
+        return get_instance()->input->server($key);
+    }
+
+    /**
+     * Shortens $this->load->view($file)
+     */
+    function loadView($file){
+        get_instance()->load->view($file);
+    }
+?>
