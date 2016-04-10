1061c8f4744e8076edefe26a3f6e63a92e44930d
Sevki Bekir Kocadag <bekirsevki@gmail.com>
2016-04-11 02:56:06 +0300

    formed courseList, course

---
+++ application/models/Courses.php
@@ -0,0 +1,58 @@
+
+<?php
+   /**
+     * SemTech Co -> E-Learning Project
+     * @2016
+     * ************ T E A M ************
+     * Şevki KOCADAĞ -> bekirsevki@gmail.com
+     * Asim Dogan NAMLI -> asim.dogan.namli@gmail.com
+     * Okan KAYA -> okankaya93@gmail.com
+     * 
+     *
+     * COURSES MODEL
+     */
+	 
+
+    class courses extends EL_Model {
+
+        /**
+         * Columns of table users
+         */
+		public $name;
+		public $instructorId;
+		public $catagoryId;
+		public $price;
+		public $createdDate;
+		public $updatedDate;
+		public $isActive;
+
+
+        public function __construct(){
+            // Call the CI_Model constructor
+            parent::__construct();
+        }
+		/**
+         * Get CourseDetails Table Inner Join
+         */
+        
+        public function getCourseDetails($id,$where = NULL){
+        	$table=$this->table;
+	        $this->db->select('*')
+	        		 ->from($this->table.' c')
+					 ->join('courseDetails cd','cd.courseId=c.id');
+	        if($where != NULL)
+	        	$this->db->where($where);
+	        $query=$this->db->get();
+	        
+	        
+	        foreach ($query->result() as $row){
+			    new dBug($row);
+			    
+			}
+        } 
+        
+    }
+?>
+                
+                
+                
