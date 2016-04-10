b931e87450dd6e4af9df3f6b1827438ad317f67e
Sevki Bekir Kocadag <bekirsevki@gmail.com>
2016-04-10 03:41:19 +0300

    new file added and rename Folder name DuzenlenmisTema

---
+++ duzenlenmisTema/connectionDB.php
@@ -0,0 +1,21 @@
+
+<?php
+//ini_set('display_errors',1);
+//error_reporting(E_ALL);
+$dbhost = "localhost";
+$dbname = "psevkik5_school";
+$dbuser = "psevkik5_usScho";
+$dbpass = "o6yk0oblltGEdJCJ";
+
+include_once "class.php";
+$password_decrypt = Encryption::decrypt($dbpass);
+
+//Datebase Connections
+
+$dataBus = mysql_connect("$dbhost", "$dbuser","$password_decrypt");
+if ( ! $dataBus ) die ("MySQL Error ! Please Report to bekirsevki@gmail.com");
+
+mysql_select_db("$dbname" , $dataBus) or die ("Database connection Failed! Please Report to bekirsevki@gmail.com" . mysql_error() );
+
+mysql_query("SET NAMES 'utf8'"); 
+?>
---
+++ duzenlenmisTema/lesson.php
@@ -0,0 +1,103 @@
+<?php include_once "sessions.php"; ?>
+<?php include_once "header.php"; 
+	
+	$getLessonId="";
+	$getLessonId=$_GET['lessonId'];
+	
+	include_once "connectionDB";
+	
+	$query="select count(*) as OK, c.name,c.id,c.teacherId from lessons l join courseToUser cTU on cTU.courseId=l.courseId  join courses c on c.Id=l.courseId where cTU.userId=$userId and l.id=$getLessonId";
+	$query=mysql_real_escape_string($query);
+	$runQuery=mysql_query($query);
+	while($fetchQuery=mysql_fetch_array($runQuery))
+	{
+		$countLesson=$fetchQuery["OK"];
+		$courseName=$fetchQuery["name"];
+		$courseId=$fetchQuery["id"];
+		$courseTeacherId=$fetchQuery["teacherId"];
+	}
+	if($courseTeacherId==$userId)
+		$countLesson=1;
+	
+	if($getLessonId=="" || $countLesson!=1)
+		header("Location:courseList.php");
+		
+		
+	$queryLesson="select l.name, l.content, lT.name as lessonTName, l.fileURL from lessons l inner join lessonTypes lT on lT.id=l.typeId where l.id=$getLessonId";
+	$queryLesson=mysql_real_escape_string($queryLesson);
+	$runQueryLesson=mysql_query($queryLesson);
+	while($fetchQueryLesson=mysql_fetch_array($runQueryLesson))
+	{
+		$lessonName=$fetchQueryLesson["name"];
+		$lessonContent=$fetchQueryLesson["content"];
+		$lessonTypeName=$fetchQueryLesson["lessonTName"];
+		$lessonFileURL=$fetchQueryLesson["fileURL"];
+	}
+?>
+<section id="sub-header" >
+  	<div class="container">
+    
+    	<div class="row">
+        	<div class="col-md-12 text-center">
+            	<h1><?php echo $lessonName; ?></h1>
+                
+            </div>
+        </div><!-- End row -->
+        
+        
+    </div><!-- End container -->
+    <div class="divider_top"></div>
+</section>
+
+
+  
+  <section id="main_content">
+  <div class="container">
+  
+<ol class="breadcrumb">
+      <li><a href="courseList">All Courses</a></li>
+      <li><a href=" <?php assetsUrl(); ?>course.php?courseId=<?php echo $courseId; ?>"><?php echo $courseName; ?></a></li>
+      <li class="active"><?php echo $lessonName; ?></li>
+</ol>
+
+	 <div class="row">
+     		<div class="col-md-12">
+     		<hr>
+			<?php 
+			if($lessonTypeName=="Video")
+			{	
+				 echo "<iframe src='$lessonFileURL' style='border:0;' class='video_course' allowfullscreen></iframe><br/>";
+			}
+			echo $lessonContent;
+			          
+           
+                
+                
+                //$server=$_SERVER['HTTP_HOST'];
+                //$direction=rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+                //$targetPage="index.php";
+                //$server.$direction."/".$targetPage;
+                //header("Location: http://$server$direction/$targetPage");
+                //echo "<meta http-equiv='refresh' content='0; url=http://$server$direction/$targetPage'>";
+                
+            $query="select * from lessonProgress where userId=$userId and lessonId=$getLessonId";
+            $query=mysql_real_escape_string($query);
+			$runQuery=mysql_query($query);
+			while($fetchQuery=mysql_fetch_array($runQuery))
+				$lessonLegendId=$fetchQuery["lessonLegendId"];
+            ?>
+            <div class="clearfix text-center"><a href=" <?php assetsUrl(); ?>process_completion.php?lessonId=<?php echo $getLessonId; ?>" class="button_medium_outline <?php if($lessonLegendId==2) echo "btn disabled" ?>">Mark as Complete <?php if($lessonLegendId==2) echo "<i class='icon-ok'></i>" ?></a>  </div>
+            </div><!-- End col-md-12  -->
+            
+           
+     	
+     </div><!-- End row -->
+  </div><!-- End container -->
+  </section>
+
+
+
+<?php include "footer.php"; ?>
+
+  </body>
+</html>
