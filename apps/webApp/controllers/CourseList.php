0dadcc2f247813559ab38ad3bec5d3d7f2342e05
SevkiBekir <bekirsevki@gmail.com>
2016-07-19 15:22:16 +0300

    SEM sunucu

--- apps/webApp/controllers/CourseList.php
+++ apps/webApp/controllers/CourseList.php
@@ -16,30 +16,46 @@ class courseList extends CI_Controller {
 		$this->load->model('courses');
     
         
-        
-        $data=$this->courses->getCourseDetails(NULL,array('isActive'=>1));
+        //COURSE DATA
+        $get=$this->courses->getCourseDetails(NULL,array('isActive'=>1));
 		//new dBug($data);
 		$i=0;
-		foreach($data as $row){
+        $data = [];
+		foreach($get as $row){
 			$getCatagoryName=$this->courses-> getCatagoryName($row->catagoryId);
 			$getDateDifference=$this->courses->getDateDifference($row->id);
-	        $data[$i]=array('courseName'=>$row->name,
+			$getCourseRating=$this->courses->getCourseRating($row->id);
+	        $data['a'.$i]=array('courseName'=>$row->name,
 	        			'courseSummary'=>$row->summary,
 	        			'courseUpdatedDate'=>$row->updatedDate,
 	        			'courseCatagoryName'=>$getCatagoryName->name,
 	        			'coursePrice'=>$row->price,
 	        			'courseDateDifference'=>$getDateDifference->days,
-						
+						'courseRating'=>intval($getCourseRating->stars),
 	        								);
+	        
 			$i++;
 		}
+		$myA = array('courseData'=> $data);
+		$myA['countCourse']=array('count'=>$i);
 		
 		
-        new dBug($data);
-       
-
-        
-        loadView('courseList',$data);
+		$getCatagory=$this->courses->getCatagory();
+		new dBug($getCatagory);
+		$i=0;
+		$data=[];
+		foreach($getCatagory as $row){
+			$data['c'.$i]=array('catagoryId'=>$row->id,
+								'catagoryName'=>$row->name
+								);
+			$i++;
+		}
+		$myA['catagories']=$data;
+		
+		
+		new dBug($myA);
+		loadView('courseList',$myA);
+        loadView('footer');
         
 	}
 }
